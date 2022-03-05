95a28755c1e34af3f112c84df2a0cb96c00ca105
Drew Wilson <buzzinbubba@gmail.com>
2022-03-05 15:14:26 -0700

    grocery update

---
+++ grocery/src/router/index.js
@@ -0,0 +1,35 @@
+//configure which view to use for each URL
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Browse from '../views/BrowseView.vue'
+import CartView from '../views/CartView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/browse',
+    name: 'Browse',
+    component: Browse
+  },
+  {
+    path: '/cart',
+    name: "Cart",
+    component: CartView
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
